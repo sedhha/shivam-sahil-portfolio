bccbb3ce97555cf147cf6bcf4efdb091ab80139d
sedhha <activity.schoolsh2@gmail.com>
2021-08-07 18:16:08 +0530

    My Portfolio Website 02

---
+++ src/features/homePage/constants/index.js
@@ -0,0 +1,7 @@
+export { topHeaders } from "./header/topHeader";
+
+export { mainContent } from "./body/mainContent";
+
+export { navItemsHeader } from "./miscMappings";
+
+export { footerData } from "./footer/footer";
