a3f9cccdc6b372f616d9fa8a62d2b3c82de9fcd3
sedhha <activity.schoolsh2@gmail.com>
2021-08-08 15:46:34 +0530

    Updated to record user interaction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from "react";
 import HomePageSkeleton from "./features/homePage/skeleton";
 import "./app.scss";
-
+import ReactGa from "react-ga";
+import axios from "axios";
 function App() {
+	React.useEffect(() => {
+		// console.log(process.env);
+		ReactGa.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+		ReactGa.pageview("/");
+		const getData = async () => {
+			//const res = await fetch("https://geolocation-db.com/json/");
+
+			axios.get("https://geolocation-db.com/json/").then(response => {
+				console.log("Response = ", response);
+				axios({
+					method: "post",
+					url: process.env.REACT_APP_SHEETS_URI,
+					data: {
+						apiKey: process.env.REACT_APP_SHEETS_API_KEY,
+						operationType: process.env.REACT_APP_UPDATE_WEBSITE_PING,
+						operationData: response.data,
+					},
+					headers: {
+						"Content-Type": "text/plain;charset=utf-8",
+					},
+				})
+					.then(function (response) {
+						console.log(response);
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			});
+		};
+		getData();
+	}, []);
 	return (
 		<div>
 			<HomePageSkeleton />
