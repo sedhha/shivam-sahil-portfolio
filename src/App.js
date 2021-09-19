eb09ddf809d0a0e9b988baa713aab791aa763891
sedhha <activity.schoolsh2@gmail.com>
2021-09-20 01:30:21 +0530

    Removed GA

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import 'react-slidedown/lib/slidedown.css';
 import HomePageSkeleton from './features/homePage/skeleton';
 import './app.scss';
-import ReactGa from 'react-ga';
-// import axios from 'axios';
-// import io from 'socket.io-client';
-// import { endpoint } from './endpoint';
 
-// const socket = io.connect(endpoint, {
-//   'sync disconnect on unload': true,
-//   'query': { token: process.env.REACT_APP_HEROKU_KEY },
-// });
 function App() {
-  React.useEffect(() => {
-    ReactGa.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-    ReactGa.pageview('/');
-  }, []);
+  // React.useEffect(() => {
+  //   ReactGa.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
+  //   ReactGa.pageview('/');
+  // }, []);
   return (
     <div>
       <HomePageSkeleton />
@@ -24,59 +16,3 @@ function App() {
 }
 
 export default App;
-
-/*import React from 'react';
-import 'react-slidedown/lib/slidedown.css';
-import HomePageSkeleton from './features/homePage/skeleton';
-import './app.scss';
-import ReactGa from 'react-ga';
-import axios from 'axios';
-import io from 'socket.io-client';
-import { endpoint } from './endpoint';
-const socket = io.connect(endpoint, {
-  'sync disconnect on unload': true,
-  'token': process.env.REACT_APP_HEROKU_KEY,
-});
-socket.on('message-response', (msg) =>
-  console.log('Response recieved = ', msg)
-);
-function App() {
-  React.useEffect(() => {
-    ReactGa.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
-    ReactGa.pageview('/');
-    const getData = async () => {
-      //const res = await fetch("https://geolocation-db.com/json/");
-      if (process.env.NODE_ENV === 'production')
-        axios.get('https://geolocation-db.com/json/').then((response) => {
-          axios({
-            method: 'post',
-            url: process.env.REACT_APP_SHEETS_URI,
-            data: {
-              apiKey: process.env.REACT_APP_SHEETS_API_KEY,
-              operationType: process.env.REACT_APP_UPDATE_WEBSITE_PING,
-              operationData: response.data,
-            },
-            headers: {
-              'Content-Type': 'text/plain;charset=utf-8',
-            },
-          })
-            .then(function () {})
-            .catch(function (error) {
-              console.log('Error Occured = ', error);
-            });
-        });
-    };
-    getData();
-  }, []);
-  const onClickEmitter = () => {
-    socket.emit('message', { shivam: 'Test Success' });
-  };
-  return (
-    <div>
-      <button onClick={onClickEmitter}>Click and Test</button>
-      <HomePageSkeleton />
-    </div>
-  );
-}
-
-export default App;*/
