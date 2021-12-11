40c59117ec28020350bb06a8bfd30687575e59d7
sedhha <activity.schoolsh2@gmail.com>
2021-12-11 11:44:47 +0530

    Webp Shift: My Expertise: BG image

--- src/features/homePage/constants/body/sections/sectionFeatures.js
+++ src/features/homePage/constants/body/sections/sectionFeatures.js
@@ -1,78 +1,80 @@
-import { nanoid } from "nanoid";
-import { sectionIds } from "../../miscMappings";
-import { configStyles } from "../constants/cssColors";
-import { getIcon, iconsMap } from "../../miscMappings";
+import { nanoid } from 'nanoid';
+import { sectionIds } from '../../miscMappings';
+import { configStyles } from '../constants/cssColors';
+import { getIcon, iconsMap } from '../../miscMappings';
 
 const iconStyling = {
-	//For styling the React Icons with gradient colors, Read Only
-	style: {
-		stroke: "url(#sectionFeatures__sections__0__displayIcon__Blockchain)", // Not to be changed
-		fill: "url(#sectionFeatures__sections__0__displayIcon__Blockchain)", //Not to be changed
-	},
+  //For styling the React Icons with gradient colors, Read Only
+  style: {
+    stroke: 'url(#sectionFeatures__sections__0__displayIcon__Blockchain)', // Not to be changed
+    fill: 'url(#sectionFeatures__sections__0__displayIcon__Blockchain)', //Not to be changed
+  },
 };
 
 export const sectionFeatures = {
-	showSection: true,
-	imageGrade1: "rgba(126,213,111,.8)", //For the image gradient of second section
-	imageGrade2: "rgba(40,180,133,.8)",
-	headImageUrl: "web/images/hero4.jpg",
-	sectionHeading: "Feathers in my Cap", // The top Display Section
-	sectionFeatures_sectionId: sectionIds.sectionFeatures_sectionId,
-	sections: [
-		{
-			displayIconStyle: configStyles.defaults.displayIconStyle,
-			id: "sections__" + nanoid(),
-			displayIconId: "sectionFeatures__sections__0__displayIcon__Blockchain", //This ID must be present in display
-			//icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
-			// property isn't required
-			displayIcon: getIcon(iconsMap.reactjs, { ...iconStyling }),
-			heading: "Mern Stack",
-			headingStyle: {
-				marginBottom: "1.5rem",
-			},
-			para: "Analytics Applications, Configuration UI, Open Source Portfolios, etc.",
-		},
+  showSection: true,
+  imageGrade1: 'rgba(126,213,111,.8)', //For the image gradient of second section
+  imageGrade2: 'rgba(40,180,133,.8)',
+  headImageUrlIsWeb: true,
+  headImageUrl:
+    'https://github.com/sedhha/gitpractice/blob/master/webpf/web/images/hero4.jpg',
+  sectionHeading: 'Feathers in my Cap', // The top Display Section
+  sectionFeatures_sectionId: sectionIds.sectionFeatures_sectionId,
+  sections: [
+    {
+      displayIconStyle: configStyles.defaults.displayIconStyle,
+      id: 'sections__' + nanoid(),
+      displayIconId: 'sectionFeatures__sections__0__displayIcon__Blockchain', //This ID must be present in display
+      //icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
+      // property isn't required
+      displayIcon: getIcon(iconsMap.reactjs, { ...iconStyling }),
+      heading: 'Mern Stack',
+      headingStyle: {
+        marginBottom: '1.5rem',
+      },
+      para: 'Analytics Applications, Configuration UI, Open Source Portfolios, etc.',
+    },
 
-		{
-			displayIconStyle: configStyles.defaults.displayIconStyle,
-			id: "sections__" + nanoid(),
-			displayIconId: "sectionFeatures__sections__0__displayIcon__Blockchain", //This ID must be present in display
-			//icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
-			// property isn't required
-			displayIcon: getIcon(iconsMap.database, { ...iconStyling }),
-			heading: "Blockchain",
-			headingStyle: {
-				marginBottom: "1.5rem",
-			},
-			para: "Built decentralized finance, smart contracts and entire blockchain network using python and flask.",
-		},
+    {
+      displayIconStyle: configStyles.defaults.displayIconStyle,
+      id: 'sections__' + nanoid(),
+      displayIconId: 'sectionFeatures__sections__0__displayIcon__Blockchain', //This ID must be present in display
+      //icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
+      // property isn't required
+      displayIcon: getIcon(iconsMap.database, { ...iconStyling }),
+      heading: 'Blockchain',
+      headingStyle: {
+        marginBottom: '1.5rem',
+      },
+      para: 'Built decentralized finance, smart contracts and entire blockchain network using python and flask.',
+    },
 
-		{
-			displayIconStyle: configStyles.defaults.displayIconStyle,
-			id: "sections__" + nanoid(),
-			displayIconId: "sectionFeatures__sections__0__displayIcon__Blockchain", //This ID must be present in display
-			//icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
-			// property isn't required
-			displayIcon: getIcon(iconsMap.robot, { ...iconStyling }),
-			heading: "Robotics",
-			headingStyle: {
-				marginBottom: "1.5rem",
-			},
-			para: "CAD modellnig, rapid manufacturing, microcontroller programming, IoT and socket communication.",
-		},
+    {
+      displayIconStyle: configStyles.defaults.displayIconStyle,
+      id: 'sections__' + nanoid(),
+      displayIconId: 'sectionFeatures__sections__0__displayIcon__Blockchain', //This ID must be present in display
+      //icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
+      // property isn't required
+      displayIcon: getIcon(iconsMap.robot, { ...iconStyling }),
+      heading: 'Robotics',
+      headingStyle: {
+        marginBottom: '1.5rem',
+      },
+      para: 'CAD modellnig, rapid manufacturing, microcontroller programming, IoT and socket communication.',
+    },
 
-		{
-			displayIconStyle: configStyles.defaults.displayIconStyle,
-			id: "sections__" + nanoid(),
-			displayIconId: "sectionFeatures__sections__0__displayIcon__Blockchain", //This ID must be present in display
-			//icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
-			// property isn't required
-			displayIcon: getIcon(iconsMap.eye, { ...iconStyling }),
-			heading: "Computer Vision",
-			headingStyle: {
-				marginBottom: "1.5rem",
-			},
-			para: "Background removal, SIFT and SURF Feature extraction, Real time image socket transmission etc.",
-		},
-	],
+    {
+      displayIconStyle: configStyles.defaults.displayIconStyle,
+      id: 'sections__' + nanoid(),
+      displayIconId: 'sectionFeatures__sections__0__displayIcon__Blockchain', //This ID must be present in display
+      //icon styling in stroke and fill. Make sure to keep it unique in case you're using id. Other wise this
+      // property isn't required
+      displayIcon: getIcon(iconsMap.eye, { ...iconStyling }),
+      heading: 'Computer Vision',
+      headingStyle: {
+        marginBottom: '1.5rem',
+      },
+      para: 'Background removal, SIFT and SURF Feature extraction, Real time image socket transmission etc.',
+    },
+  ],
 };
